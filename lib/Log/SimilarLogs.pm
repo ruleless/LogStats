4af3c803eda6cc4013de9807168bca87280a6e3a
ruleless <ruleless@126.com>
2022-04-14 10:40:05 +0800

    完善SimilarLogs的实现(未完成)

--- lib/Log/SimilarLogs.pm
+++ lib/Log/SimilarLogs.pm
@@ -3,8 +3,10 @@ package Log::SimilarLogs;
 use strict;
 use warnings;
 
+use Log::LevenDist qw(similarity_of_str);
+
 # 相似度阈值，大于此相似度的日志才能通过相似度测试
-use constant THRESHOLD_SIMIMLARITY => 0.9;
+use constant THRESHOLD_SIMIMLARITY => 0.7;
 
 # 最大能记录的日志条数
 use constant MAX_LOGS => 100;
@@ -13,7 +15,7 @@ sub new {
     my $class = shift;
     my $self = {
         # 总共的日志条数
-        total_logs => 0,
+        log_count => 0,
 
         # 具体的日志记录
         logs => [
@@ -27,20 +29,49 @@ sub new {
     bless $self, $class;
 }
 
+sub get_log_count {
+    my $self = shift;
+    return $self->{log_count};
+}
+
+sub empty {
+    my $self = shift;
+    return $self->get_log_count == 0;
+}
+
+sub for_each {
+    my ($self, $fn) = @_;
+    die 'expect callback' unless ref($fn) == ref(sub {});
+
+    foreach (@{$self->{logs}}) {
+        my $continue_or_not = $fn->($_);
+        last unless $continue_or_not;
+    }
+}
+
 sub test {
-    my ($self, $log_content, $full_log) = @_;
-    die 'invalid parameters' unless defined($log_content) && defined($full_log);
+    my ($self, $log_content) = @_;
+    die 'invalid parameters' unless defined($log_content);
+
+    if ($self->empty) {
+        return 1;
+    }
 
     foreach my $alog (@{$self->{logs}}) {
-        print "$alog->{log_content}\n";
+        my $similarity = similarity_of_str($log_content, $alog->{log_content});
+        if ($similarity < THRESHOLD_SIMIMLARITY) {
+            return 0; # 与集合中的某条不相似
+        }
     }
+
+    return 1;
 }
 
 sub add {
     my ($self, $log_content, $full_log) = @_;
     die 'invalid parameters' unless defined($log_content) && defined($full_log);
 
-    if ($self->{total_logs}++ >= MAX_LOGS) {
+    if ($self->{log_count}++ >= MAX_LOGS) {
         return;
     }
 
--- t/SimilarLogs.t
+++ t/SimilarLogs.t
@@ -5,8 +5,36 @@ use Test::More;
 
 BEGIN { use_ok('Log::SimilarLogs') };
 
+sub test_and_add {
+    my ($similar_logs, $log_content, $full_log) = @_;
+
+    if (!$similar_logs->test($log_content, $full_log)) {
+        return 0;
+    }
+
+    $similar_logs->add($log_content, $full_log);
+    return 1;
+}
+
 my $similar_logs = Log::SimilarLogs->new;
 
-$similar_logs->add('test', 'test');
+ok($similar_logs->empty);
+
+ok(test_and_add(
+    $similar_logs,
+    'start worker processes',
+    '2020/03/26 13:00:11 [notice] 24496#0: start worker processes'));
+
+ok(test_and_add(
+    $similar_logs,
+    'start worker process 24497',
+    '2020/03/26 13:00:11 [notice] 24496#0: start worker process 24497'));
+
+ok(test_and_add(
+    $similar_logs,
+    'start worker process 24498',
+    '2020/03/26 13:00:11 [notice] 24496#0: start worker process 24498'));
+
+is($similar_logs->get_log_count, 3);
 
 done_testing;
